6cd3d8f6e34dfb61893133359e84ad2ff18b2a13
Juan Manuel <juanmanuelfigueroavalencia@gmail.com>
2024-10-19 17:54:42 -0500

    Condiciones BackGround (Terminadas)

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ TPM = np.array([
 
 
 subconjuntoSistemaCandidato = np.array([
-    'at', 'bt', 'ct', 'at+1', 'bt+1', 'ct+1'
+    'at','bt', 'ct','at+1', 'bt+1', 'ct+1'
 ])
 
 estadoActualElementos = np.array([
-    {'at': 1},
+    {'at': 0},
     {'bt': 0},
     {'ct': 0},
     {'dt': 0}
@@ -40,13 +40,12 @@ subconjuntoElementos = np.array(['at', 'bt', 'ct'])
 #* MATRICES INICIALES
 n = len(estadoActualElementos)
 matrizPresente = generarMatrizPresenteInicial(n)
-
 matrizFuturo = generarMatrizFuturoInicial(matrizPresente)
 
 # #* Condiciones de background
 
 #* Elementos que no hacen parte del sistema cantidato
-elementosBackground = [elemento for elemento in estadoActualElementos if next(iter(elemento)) not in subconjuntoElementos]
+elementosBackground = [elemento for elemento in reversed(estadoActualElementos) if next(iter(elemento)) not in subconjuntoElementos]
 
 nuevaTPM = np.copy(TPM)
 nuevaMatrizPresente = np.copy(matrizPresente)
@@ -59,22 +58,27 @@ nuevaMatrizFuturo = np.copy(matrizFuturo)
 #? elementosBackground: elementos del background {elemento: valor inicial}
 def aplicarCondicionesBackground(nuevaMatrizPresente, nuevaMatrizFuturo, nuevaTPM, elementosBackground):
     if len(elementosBackground) > 0:
+        #Extraemos primero los indices con su respectivo elemento del estado actual
+        # Crear un diccionario que mapea los nombres a su índice
+        indicesCondicionesBackGround = {list(elem.keys())[0]: idx for idx, elem in enumerate(estadoActualElementos)}
+        print(indicesCondicionesBackGround)
+
         for elemento in elementosBackground:
 
-            llave = list(elemento.keys())[0]
+            llave = list(elemento.keys())[0]  
 
-            indice = next((idx for idx, i in enumerate(estadoActualElementos) if llave in i), len(estadoActualElementos))
+            indice = indicesCondicionesBackGround[llave]
 
             valorActualElemento = elemento[llave]
             
             #* Ya sabemos la posicion del elementp
             #* Ahora buscamos en la matriz presente y futura la fila y columna correspondiente
-            # Si el valor actual del elemento es 0 dejamos las filas que tengan 1
-            # Si el valor actual del elemento es 1 dejamos las filas que tengan 0
+            # Si el valor actual del elemento es 0 dejamos las filas que tengan 0
+            # Si el valor actual del elemento es 1 dejamos las filas que tengan 1
             for i in range(len(nuevaMatrizPresente)):
-                if nuevaMatrizPresente[i][indice] == 0 if valorActualElemento == 0 else 1:
+                if nuevaMatrizPresente[i][indice] == (1 if valorActualElemento == 0 else 0):
                     nuevaMatrizPresente[i].fill(99)
-
+                    
             filas_a_eliminar = []
             for i in range(len(nuevaMatrizPresente)):
                 if 99 in nuevaMatrizPresente[i]:
